74f2318e38c06e6f6c8c3d72b7429d5d180b3abc
SebastianK2000 <sebastian.kosciolek2000@gmail.com>
2024-07-29 12:37:10 +0200

    complete login and register form add validation and full responsive

--- loginForm/src/components/login/Login.tsx
+++ loginForm/src/components/login/Login.tsx
@@ -1,16 +1,68 @@
 import '../../style/login.css';
 import { Link } from 'react-router-dom';
+import FormInput from '../register/formInput';
+import { useState } from "react";
 
 const LoginForm = () => {
+  const [values, setValues] = useState<{
+    [key: string]: String;
+  }>({
+    email: "",
+    password: "",
+  });
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const inputs = [
+    {
+      id: 1,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "It should be a valid email address!",
+      label: "Email",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "password",
+      type: showPassword ? "text" : "password",
+      placeholder: "Password",
+      label: "Password",
+      required: true,
+    }
+  ]
+
+  const onChange = (e: any) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className="main-container-login-form">
-      <form >
+      <form className="login-main-form">
         <h1>Login</h1>
         <p className="do-you-have-account">Don't have an account? <Link className="link-react-decoration-none" to="/register"><b className="register-here">Register here!</b></Link></p>
-          <div className="formInput">
-            <label></label>
-            <input />
-          </div>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <div className="show-password-container">
+          <input
+            type="checkbox"
+            id="showPassword"
+            checked={showPassword}
+            onChange={toggleShowPassword}
+          />
+          <label htmlFor="showPassword">Show Password</label>
+        </div>
         <button>Submit</button>
       </form>
     </div>
